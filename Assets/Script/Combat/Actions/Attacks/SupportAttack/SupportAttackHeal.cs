57312f16fdf949e25f7f09f020a43372a3250c17
Thomas-DaPrato <daprato.thomas05@gmail.com>
2022-12-15 07:41:54 +0100

    fautes d'orthographes

--- Assets/Script/Combat/Actions/Attacks/SupportAttack/SupportAttackHeal.cs
+++ Assets/Script/Combat/Actions/Attacks/SupportAttack/SupportAttackHeal.cs
@@ -8,6 +8,8 @@ public class SupportAttackHeal : Actions
     public override void DoAction()
     {
         foreach(Slider slider in FightManager.enemiesHP){
+            if (slider == null)
+                continue;
             if(slider.value != 0)
                 slider.value += 3;
         }
